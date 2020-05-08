015d30343d77cf8577d67fb24d6d23d80841dac5
Trung iOS <trungnt@vps.com.vn>
2020-05-08 21:57:28 +0700

    Initial Commit

---
+++ CineWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CineWorld
+//
+//  Created by Trung iOS on 5/8/20.
+//  Copyright © 2020 Fighting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
