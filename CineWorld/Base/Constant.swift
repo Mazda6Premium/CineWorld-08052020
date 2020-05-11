4fa222e8d87ec23a247d30f9387bdaf811221dee
Trung <nttrung254@gmail.com>
2020-05-11 23:48:02 +0700

    add icon

--- CineWorld/Base/Constant.swift
+++ CineWorld/Base/Constant.swift
@@ -10,5 +10,5 @@ import UIKit
 
 let ROUND_CORNER_VIEW_POPUP: CGFloat = 10
 let ROUND_CORNER_BUTTON: CGFloat = 8
-
 let BORDER_IMAGE_COLOR: CGColor = #colorLiteral(red: 0.370555222, green: 0.3705646992, blue: 0.3705595732, alpha: 1)
+let MAIN_COLOR: CGColor = #colorLiteral(red: 0.3137254902, green: 0.01568627451, blue: 0.6, alpha: 1)
